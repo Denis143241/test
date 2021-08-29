7e4e7137d1d4db713a5a1787e89136d50ea25a7c
Denis <d.igoschin.igoshin@gmail.com>
2021-08-29 09:26:20 +0300

    init 2

---
+++ precache-manifest.d36626a4821d672317231f189ae943f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46f2c80ef661125489bb",
+    "url": "/css/app.b0cb4bc8.css"
+  },
+  {
+    "revision": "a8075c954fad729d859c7f02f9d379c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46f2c80ef661125489bb",
+    "url": "/js/app.442602b2.js"
+  },
+  {
+    "revision": "83e0ed2ce5973fb713c6",
+    "url": "/js/chunk-vendors.0ec55f4a.js"
+  },
+  {
+    "revision": "d6f7e032577adff4f9bb617d7f3f7a38",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
